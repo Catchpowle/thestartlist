06d453367376f1d4c4efa67e7ff815e1a0c6fc9c
eanplatter <ean.platter@gmail.com>
2013-11-22 00:57:44 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Startlist::Application.config.secret_key_base = 'bbf889d90b00b4aab795fc68b2bd5acb161ff070ccb8c1cc3602ec90b40f64be96f19b39a37abbf7000797840c1720dc3ff1ecaac74f779b788f464b6312871c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Startlist::Application.config.session_store :cookie_store, key: '_startlist_session'
